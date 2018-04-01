d6873a6096fac2ff63fe743a4fad85896507a7c2
mtKhine <mtkhinejas@gmail.com>
2018-04-01 07:32:00 -0700

    3 Day forecast added

--- smart_glass_arduino.ino
+++ smart_glass_arduino.ino
@@ -4,6 +4,8 @@
 #include <WiFiUdp.h>
 #include <time.h>
 #include "stationCredentials.h"
+long DELAY_ERROR = 5 * 60 * 1000;
+long DELAY_NORMAL = 20 * 60 * 1000;
 
 WiFiClient client;
 float timezone = 6.5;
@@ -23,10 +25,12 @@ String country = "";
 String mainCondition = "";
 String weatherDescription = "";
 float temperature = 0;
-float humidity = 0;
+String humidity;
 float pressure = 0;
 
+
 int counter = 300;
+
 
 void setup() {
   delay(1000);
@@ -42,17 +46,17 @@ void loop() {
 
   //updating weather data
   if (counter == 300) {
-    //pull from server in 5 minutes interval
+    //pull from server in every 5 minutes
     counter = 0;
     Serial.println("New");
-    getWeatherData();
+    getWeatherDataFromWUnderGround(WUNDERGROUND_FORECAST_REQ, "forecast");
     displayCurrentWeather();
   } else {
     counter++;
-    Serial.println("Old");
-    displayPreviousWeather( weatherLocation,
-                            country, mainCondition, weatherDescription,
-                            temperature, humidity, pressure);
+    //    Serial.println("Old");
+    //    displayPreviousWeather( weatherLocation,
+    //                            country, mainCondition, weatherDescription,
+    //                            temperature, humidity, pressure);
   }
 
   //updating time
@@ -72,11 +76,11 @@ void loop() {
   displayCurrentDate();
 }
 
-void getWeatherData() {
+void getWeatherDataFromOpenWeatherMap() {
 
   //connect api
   if (client.connect(SERVER, 80)) {
-    client.println(currentWeather + "q=" + cityName + "&units=metric&APPID=" + API_KEY);
+    client.println(currentWeather + "id=" + cityId + "&units=metric&APPID=" + API_KEY);
     client.println("Host: api.openweathermap.org");
     client.println("User-Agent: ArduinoWiFi/1.1");
     client.println("Connection: close");
@@ -115,7 +119,7 @@ void getWeatherData() {
   String main = root["weather"]["main"];
   String description = root["weather"]["description"];
   float temp = root["main"]["temp"];
-  float humid = root["main"]["humidity"];
+  //float humid = root["main"]["humidity"];
   float pres = root["main"]["pressure"];
 
   weatherLocation = location;
@@ -123,9 +127,156 @@ void getWeatherData() {
   mainCondition = main;
   weatherDescription = description;
   temperature = temp;
-  humidity = humid;
+  //humidity = humid;
   pressure = pres;
 
+}
+
+void getWeatherDataFromWUnderGround(const char *request, String type) {
+  //connect api
+  if (client.connect(WUNDERGROUND_SERVER, 80)) {
+    client.print(request);
+    client.flush();
+  } else {
+    Serial.println("connection failed");
+    Serial.println();
+  }
+
+  int respLen = 0;
+  static char respBuf[4096];
+  bool skip_headers = true;
+  while (client.connected() || client.available()) {
+    if (skip_headers) {
+      String aLine = client.readStringUntil('\n');
+      //Serial.println(aLine);
+      // Blank line denotes end of headers
+      if (aLine.length() <= 1) {
+        skip_headers = false;
+      }
+    }
+    else {
+      int bytesIn;
+      bytesIn = client.read((uint8_t *)&respBuf[respLen], sizeof(respBuf) - respLen);
+      //Serial.print(F("bytesIn ")); Serial.println(bytesIn);
+      if (bytesIn > 0) {
+        respLen += bytesIn;
+        if (respLen > sizeof(respBuf)) respLen = sizeof(respBuf);
+      }
+      else if (bytesIn < 0) {
+        Serial.print(F("read error "));
+        Serial.println(bytesIn);
+      }
+    }
+    delay(1);
+  }
+  client.stop();
+
+  if (respLen >= sizeof(respBuf)) {
+    Serial.print(F("respBuf overflow "));
+    Serial.println(respLen);
+    delay(DELAY_ERROR);
+    return;
+  }
+  // Terminate the C string
+  respBuf[respLen++] = '\0';
+  //  Serial.print(F("respLen "));
+  //  Serial.println(respLen);
+
+  if (type == "current") {
+    extractCurrentFields(respBuf);
+  } else if (type == "forecast") {
+    extractForecastFields(respBuf);
+  }
+}
+
+bool extractCurrentFields(char *json)
+{
+  StaticJsonBuffer<3 * 1024> jsonBuffer;
+
+  // Skip characters until first '{' found
+  // Ignore chunked length, if present
+  char *jsonstart = strchr(json, '{');
+  //Serial.print(F("jsonstart ")); Serial.println(jsonstart);
+  if (jsonstart == NULL) {
+    Serial.println(F("JSON data missing"));
+    return false;
+  }
+  json = jsonstart;
+
+  // Parse JSON
+  JsonObject& root = jsonBuffer.parseObject(json);
+  if (!root.success()) {
+    Serial.println(F("jsonBuffer.parseObject() failed"));
+    return false;
+  }
+
+  // Extract weather info from parsed JSON
+  JsonObject& current = root["current_observation"];
+  JsonObject& location = root["current_observation"]["display_location"];
+  const float temp_c = current["temp_c"];
+  const char *humi = current[F("relative_humidity")];
+  const char *weather = current["weather"];
+  const float pressure_in = current["pressure_in"];
+  const char *description = current["icon_url"];
+  const char *city = location["city"];
+  const char *coun = location["state_name"];
+
+  weatherLocation = city;
+  country = coun;
+  mainCondition = weather;
+  weatherDescription = description;
+  temperature = temp_c;
+  humidity = humi;
+  pressure = pressure_in;
+
+  return true;
+}
+
+void extractForecastFields(char *json) {
+  StaticJsonBuffer<3 * 1024> jsonBuffer;
+
+  //just filter
+  char *jsonStart = strchr(json, '{');
+  if (jsonStart == NULL) {
+    Serial.println(F("JSON data missing"));
+  }
+  json = jsonStart;
+  JsonObject& root = jsonBuffer.parseObject(json);
+  if (!root.success()) {
+    Serial.println(F("jsonBuffer.parseObject() failed"));
+  }
+
+  // Extract weather info from parsed JSON
+  JsonObject& forecast = root["forecast"]["simpleforecast"]["forecastday"];
+  JsonObject& firstDay = forecast["1"];
+  JsonObject& secondDay = forecast["2"];
+  JsonObject& thirdDay = forecast["3"];
+
+  //first day forecast
+  char weekDay=firstDay["date"]["weekday_short"];
+  float max_temp=firstDay["high"]["celsius"];
+  float min_temp=firstDay["low"]["celsius"];
+  char condition=firstDay["conditions"];
+  char icon=firstDay["icon_url"];
+  
+  //second day forecast
+  //first day forecast
+  char weekDay1=secondDay["date"]["weekday_short"];
+  float max_temp1=secondDay["high"]["celsius"];
+  float min_temp1=secondDay["low"]["celsius"];
+  char condition1=secondDay["conditions"];
+  char icon1=secondDay["icon_url"];
+  
+  //thirday forecast
+  char weekDay2=thirdDay["date"]["weekday_short"];
+  float max_temp2=thirdDay["high"]["celsius"];
+  float min_temp2=thirdDay["low"]["celsius"];
+  char condition2=thirdDay["conditions"];
+  char icon2=thirdDay["icon_url"];
+
+  displayForecastWeather(weekDay,max_temp,min_temp,condition,icon);
+  displayForecastWeather(weekDay1,max_temp1,min_temp1,condition1,icon1);
+  displayForecastWeather(weekDay2,max_temp2,min_temp2,condition2,icon2);
 }
 
 void displayWifiStatus() {
@@ -138,45 +289,6 @@ void displayWifiStatus() {
   Serial.println("WIFI has been Connected");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
-}
-void displayCurrentWeather() {
-  Serial.print("Location: ");
-  Serial.print(weatherLocation);
-  Serial.print(", ");
-  Serial.println(country);
-
-  Serial.print("Main Condition: ");
-  Serial.println(mainCondition);
-  Serial.print("Description: ");
-  Serial.println(weatherDescription);
-  Serial.print("Temperature: ");
-  Serial.println(temperature);
-  Serial.print("Humidity: ");
-  Serial.println(humidity);
-  Serial.print("Pressure: ");
-  Serial.println(pressure);
-  Serial.println();
-}
-
-void displayPreviousWeather(String location, String country, String mainCondition, String description, float temperature,
-                            float humidity, float pressure) {
-
-
-  Serial.print("Location: ");
-  Serial.print(location);
-  Serial.print(", ");
-  Serial.println(country);
-
-  Serial.print("Main Condition: ");
-  Serial.println(mainCondition);
-  Serial.print("Description: ");
-  Serial.println(description);
-  Serial.print("Temperature: ");
-  Serial.println(temperature);
-  Serial.print("Humidity: ");
-  Serial.println(humidity);
-  Serial.print("Pressure: ");
-  Serial.println(pressure);
 }
 
 void displayCurrentTime() {
@@ -201,16 +313,41 @@ void displayCurrentTime() {
 void displayCurrentDate() {
   Serial.print("Date: ");
   Serial.print(DAY[dayIndex]);
+  Serial.print(", ");
+
+  Serial.print(day);
   Serial.print(" ");
   Serial.print(MONTH[monthIndex]);
   Serial.print(" ");
   Serial.println(year);
+}
 
-  Serial.print("Date: ");
-  Serial.print(day);
-  Serial.print(".");
-  Serial.print(monthIndex);
-  Serial.print(".");
-  Serial.println(year);
+void displayCurrentWeather() {
+  Serial.print("Location: ");
+  Serial.print(weatherLocation);
+  Serial.print(", ");
+  Serial.println(country);
+
+  Serial.print("Main Condition: ");
+  Serial.println(mainCondition);
+  Serial.print("Description: ");
+  Serial.println(weatherDescription);
+  Serial.print("Temperature: ");
+  Serial.println(temperature);
+  Serial.print("Humidity: ");
+  Serial.println(humidity);
+  Serial.print("Pressure: ");
+  Serial.println(pressure);
+  Serial.println();
+}
+
+void displayForecastWeather(char weekDay,float max_temp,float min_temp,char condition,char icon){
+  Serial.println(weekDay);
+  Serial.println(condition);
+  Serial.println(icon);
+  Serial.println(max_temp);
+  Serial.print(" / ");
+  Serial.println(min_temp);
+  Serial.println();
 }
 
--- stationCredentials.h
+++ stationCredentials.h
@@ -3,7 +3,27 @@ const char* PASSWORD = "skytheodore";
 const char* SERVER="api.openweathermap.org";
 const String API_KEY="7c1219fbdb1731a3fbca8fa5d1e1ab7a";
 String cityName = "Yangon";
+String cityId="1298824";
 String currentWeather = "GET /data/2.5/weather?";
+String forecastWeather = "GET /data/2.5/forecast?";
 
 const String DAY[]={"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
 const String MONTH[]={"Jan","Feb","Mar","April","May","June","July","August","Sep","Oct","Nov","Dec"};
+
+const char* WUNDERGROUND_SERVER="api.wunderground.com";
+const String WUNDERGROUND_API_KEY="5abecca5bd642af5";
+const char WUNDERGROUND_REQ[] =
+    "GET /api/5abecca5bd642af5/conditions/q/Yangon.json HTTP/1.1\r\n"
+    "User-Agent: ESP8266/0.1\r\n"
+    "Accept: */*\r\n"
+    "Host: api.wunderground.com\r\n"
+    "Connection: close\r\n"
+    "\r\n";
+
+const char WUNDERGROUND_FORECAST_REQ[] =
+    "GET /api/5abecca5bd642af5/forecast/q/Yangon.json HTTP/1.1\r\n"
+    "User-Agent: ESP8266/0.1\r\n"
+    "Accept: */*\r\n"
+    "Host: api.wunderground.com\r\n"
+    "Connection: close\r\n"
+    "\r\n";
